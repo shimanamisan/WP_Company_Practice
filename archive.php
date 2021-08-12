462af6dee9f7a2b38ab36a7e682ab86d5aa55199
shimanamisan <mikan.sup.all@gmail.com>
2021-08-13 07:16:48 +0900

    [update] Step5-2 Pluginのページネーションと自作ページネーション関数の動作テストまで

--- archive.php
+++ archive.php
@@ -1,23 +1,31 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <div class="page-inner full-width">
-    <div
-        class="page-main"
-        id="pg-news"
-    >
-        <div class="main-container">
-            <div class="main-wrapper">
-                <div class="newsLists">
-                <!-- ループ処理 -->
-                <?php
-                  if( have_posts() ):
-                    while( have_posts() ): the_post();
-                      get_template_part('content-archive');
-                    endwhile;
-                  endif;
-                ?>
-                </div>
-            </div>
+  <div class="page-main" id="pg-news">
+    <div class="main-container">
+      <div class="main-wrapper">
+        <div class="newsLists">
+          <!-- ループ処理 -->
+          <?php
+          if (have_posts()) :
+            while (have_posts()) : the_post();
+              get_template_part('content-archive');
+              debug($wp_query->max_num_pages);
+            endwhile;
+          endif;
+          ?>
+          <div class="pager">
+            <ul class="pagerList">
+              <?php
+              if(function_exists('page_navi')){
+                page_navi();
+              }
+              ?>
+              <?php //if (function_exists("pagination")) pagination($wp_query->max_num_pages); ?>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
-<?php get_footer();?>
+<?php get_footer(); ?>
